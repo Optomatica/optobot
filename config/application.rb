54a1cadc67ef22f42f5a2cae366c9a1f1716d989
Amgad Naiem <amgad@optomatica.com>
2020-06-14 14:41:31 +0200

    enabled cors for all origins

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Myapp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:8888', 'http://localhost:3000', 'http://localhost:3001', 'https://chat.optobot.ai', 'https://optobot-test.now.sh', 'https://beta.optobot.ai'
+        origins '*'
         resource '*',
           :headers => :any,
           :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
