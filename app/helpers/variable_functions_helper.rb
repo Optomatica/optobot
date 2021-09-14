ff6ff75d880ad10e88a8c454d4188795a1800155
Mourad ElSheraey <sherieymourad1997@gmail.com>
2021-09-14 06:03:01 +0200

    Add fv,  round_nearest, pmt functions

--- app/helpers/variable_functions_helper.rb
+++ app/helpers/variable_functions_helper.rb
@@ -41,4 +41,25 @@ module VariableFunctionsHelper
     num_arr.first.to_i / num_arr.last.to_i
   end
 
+  def fv(rate, nper, pmt, pv, when_due=0)
+    rate, nper, pmt, pv, when_due = rate.to_f, nper.to_f, pmt.to_f, pv.to_f, when_due.to_f
+    if rate != 0
+        tmp = (1 + rate)**nper
+        -pv * tmp - pmt * (1 + rate * when_due) / rate * (tmp - 1)
+    else
+        -(pv + pmt * nper)
+    end
+  end
+
+  def round_nearest(x, num)
+      x, num = x.to_f, num.to_f
+      Integer((Float(x) / num).round * num)
+  end
+
+  def pmt(rate, nper, pv, fv, when_due=0)
+      rate, nper, pv, fv, when_due = rate.to_f, nper.to_f, pv.to_f, fv.to_f, when_due.to_f
+      tmp = (1 + rate)**nper
+      fact = rate != 0 ? ((1 + rate * when_due) * (tmp - 1) / rate) : nper
+      -(fv + pv * tmp) / fact
+  end
 end
