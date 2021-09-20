064b8606afe3feabcd79266ad83b16a74966a6e3
elsheraey <sherieymourad1997@gmail.com>
2021-09-20 15:17:09 +0200

    Add some variable functions for testing

--- app/helpers/variable_functions_helper.rb
+++ app/helpers/variable_functions_helper.rb
@@ -41,7 +41,8 @@ module VariableFunctionsHelper
     dividend.to_i / divisor.to_i
   end
 
-  def fv(rate, nper, pmt, pv, when_due=0)
+  # The following functions are for used test purposes by Optobot's development team and will be removed later.
+  def test_fv(rate, nper, pmt, pv, when_due=0)
     rate, nper, pmt, pv, when_due = rate.to_f, nper.to_f, pmt.to_f, pv.to_f, when_due.to_f
     if rate != 0
         tmp = (1 + rate)**nper
@@ -51,15 +52,29 @@ module VariableFunctionsHelper
     end
   end
 
-  def round_nearest(x, num)
-      x, num = x.to_f, num.to_f
-      Integer((Float(x) / num).round * num)
+  def test_pmt(rate, nper, pv, fv, when_due=0)
+    rate, nper, pv, fv, when_due = rate.to_f, nper.to_f, pv.to_f, fv.to_f, when_due.to_f
+    tmp = (1 + rate)**nper
+    fact = rate != 0 ? ((1 + rate * when_due) * (tmp - 1) / rate) : nper
+    -(fv + pv * tmp) / fact
   end
 
-  def pmt(rate, nper, pv, fv, when_due=0)
-      rate, nper, pv, fv, when_due = rate.to_f, nper.to_f, pv.to_f, fv.to_f, when_due.to_f
-      tmp = (1 + rate)**nper
-      fact = rate != 0 ? ((1 + rate * when_due) * (tmp - 1) / rate) : nper
-      -(fv + pv * tmp) / fact
+  def test_pv(rate, nper, pmt, fv, when_due=0)
+    rate, nper, pmt, fv, when_due = rate.to_f, nper.to_f, pmt.to_f, fv.to_f, when_due.to_f
+    tmp = (1 + rate)**nper
+    fact = rate != 0 ? ((1 + rate * when_due) * (tmp - 1) / rate) : nper
+    -(fv + pmt * fact) / tmp
+  end
+
+  def test_goal_house_recommended_init_invest(rate, nper, pmt, fv)
+    rate, nper, pmt, fv = rate.to_f, nper.to_f, pmt.to_f, fv.to_f
+    pv = test_pv(rate, nper, pmt, fv, 0).abs
+    Integer((Float(pv) / 50000).ceil * 50000)
+  end
+
+  def test_goal_house_recommended_monthly_invest(rate, nper, pv, fv)
+    rate, nper, pv, fv = rate.to_f, nper.to_f, pv.to_f, fv.to_f
+    pmt = test_pmt(rate, nper, pv, fv, 0).abs / 12.0
+    Integer((Float(pmt) / 50000).ceil * 50000)
   end
 end
