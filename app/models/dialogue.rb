ecb6539f3b5a255b46d4d23e4eae84c34881ed82
Amgad Naiem <amgad@optomatica.com>
2021-09-29 10:44:53 +0200

    check conditions exist

--- app/models/dialogue.rb
+++ app/models/dialogue.rb
@@ -113,7 +113,7 @@ class Dialogue < ApplicationRecord
     associations_data[:children].each do |child_name, child|
       arc = Arc.create!(parent_id: self.id, child_id: child[:id])
       arc.update!(go_next: false) if child[:go_next] == false
-      child[:conditions].each do |condition|
+      child[:conditions] && child[:conditions].each do |condition|
         variable = self.project.variables.where(name: condition[:variable_name]).last
         new_condition = Condition.new(arc_id: arc.id, variable_id: variable.id)
         option = condition[:option]
