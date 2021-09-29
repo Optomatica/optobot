dc51b1576bad893b5e231e1c3ef3f2b4b743d4a8
Amgad Naiem <amgad@optomatica.com>
2021-09-29 15:42:37 +0200

    use mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,11 +90,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
    config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :authentication => :plain,
-    :user_name      => 'apikey',
-    :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
    }
